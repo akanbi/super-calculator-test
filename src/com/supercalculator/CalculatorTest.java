2a1df8bd374506a0c45ee2dafd8af8dece1589fb
Marcio Daniel da Rosa <marciodrosa@gmail.com>
2014-10-15 12:51:55 -0300

    Fixed the text typing on edit texts.

--- src/com/supercalculator/CalculatorTest.java
+++ src/com/supercalculator/CalculatorTest.java
@@ -11,26 +11,40 @@ import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 public class CalculatorTest extends UiAutomatorTestCase {
 	
 	public void testShouldCalculate() throws UiObjectNotFoundException {
-		new UiObject(new UiSelector().className(EditText.class).instance(0)).setText("10");
-		new UiObject(new UiSelector().className(EditText.class).instance(1)).setText("5");
+		typeOnEditText("10", 0);
+		typeOnEditText("5", 1);
 		new UiObject(new UiSelector().text("Calcular")).click();
 		assertTrue("The result should be visible.", new UiObject(new UiSelector().textContains("Resultado: 10 + 5 = 15")).waitForExists(5000));
 		UiDevice.getInstance().pressBack();
 	}
 	
 	public void testShouldShowMessageWhenFirstFieldIsEmpty() throws UiObjectNotFoundException {
-		new UiObject(new UiSelector().className(EditText.class).instance(0)).setText("");
-		new UiObject(new UiSelector().className(EditText.class).instance(1)).setText("5");
+		typeOnEditText("", 0);
+		typeOnEditText("5", 1);
 		new UiObject(new UiSelector().text("Calcular")).click();
 		assertTrue("Should show a message.", new UiObject(new UiSelector().text("Por favor, preencha os dois campos.")).waitForExists(2000));
 		new UiObject(new UiSelector().text("Ok")).clearTextField();
 	}
 	
 	public void testShouldShowMessageWhenSecondFieldIsEmpty() throws UiObjectNotFoundException {
-		new UiObject(new UiSelector().className(EditText.class).instance(0)).setText("10");
-		new UiObject(new UiSelector().className(EditText.class).instance(1)).setText("");
+		typeOnEditText("10", 0);
+		typeOnEditText("", 1);
 		new UiObject(new UiSelector().text("Calcular")).click();
 		assertTrue("Should show a message.", new UiObject(new UiSelector().text("Por favor, preencha os dois campos.")).waitForExists(2000));
 		new UiObject(new UiSelector().text("Ok")).clearTextField();
+	}
+	
+	private void typeOnEditText(String text, int instance) throws UiObjectNotFoundException {
+		UiObject editText = new UiObject(new UiSelector().className(EditText.class).instance(instance));
+		eraseText(editText);
+		editText.setText(text);
+	}
+	
+	private void eraseText(UiObject uiObject) throws UiObjectNotFoundException {
+		uiObject.clickBottomRight();
+		String currentText = uiObject.getText();
+		for (int i = 0; i < currentText.length(); i++) {
+			UiDevice.getInstance().pressDelete();
+		}
 	}
 }
